7157746eb529cc859e5f3aef4464615e6ed9d80d
Trace Helms <tracehelms@gmail.com>
2023-11-08 11:03:15 -0800

    update fish kill_by_name

--- fish/functions/kill_by_name.fish
+++ fish/functions/kill_by_name.fish
@@ -1,6 +1,6 @@
 function kill_by_name
   set NAME $argv[1]
-  printf "%s" "Killing all processes that match $NAME:"
+  printf "%s\n" "Killing all processes that match '$NAME' with PIDs:"
   ps -ef | grep $NAME | awk '{print $2}'
   ps -ef | grep $NAME | awk '{print $2}' | xargs kill -9
 end
