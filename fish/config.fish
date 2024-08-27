f564b310e95dce30d1e40ac6905b12512c2ae3ec
Trace Helms <tracehelms@gmail.com>
2024-08-27 14:34:20 -0700

    update shell aliases, javascript snippets

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,19 @@ alias vim="nvim"
 alias np="vim ~/Dropbox/notes/index.md"
 alias nw="vim ~/Dropbox/notes/work.md"
 alias nj="vim ~/Dropbox/writing/journal/`date +%Y-%m-%d`.md"
+alias gco="git checkout"
+alias gp="git pull"
+alias gpso="git push origin"
+alias gcm="git commit -m"
+alias gbr="git branch"
+alias gst="git status"
+alias ghist="git log --pretty=format:'%C(yellow)%ad %C(reset)%C(green)[%h]%C(reset) | %s %C(blue)%an%C(reset) %C(bold)%C(red)%d%C(reset)' --graph --date=short"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias gdns="git diff --ignore-space-change"
+alias cddream="cd ~/code/rvoh/dream/"
+alias cdpsy="cd ~/code/rvoh/psychic/"
+alias cdcent="cd ~/code/rvoh/wellos-central/"
 
 
 #####################
@@ -46,6 +59,9 @@ source $HOME/.asdf/asdf.fish
 # Homebrew keg-only installs
 set -x PATH "/opt/homebrew/opt/postgresql@13/bin:$PATH"
 set -x PATH "/opt/homebrew/opt/redis@6.2/bin:$PATH"
+
+# eslint_d use flat eslint config file (https://github.com/mantoni/eslint_d.js/issues/287#issuecomment-2127224930)
+set -x ESLINT_USE_FLAT_CONFIG true
 
 
 ############################
